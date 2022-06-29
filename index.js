bd95a43553f34f5fc747d5161baaaaa4ec5007ea
Daniel <daniel.cuzco@gmail.com>
2022-06-29 10:08:11 -0400

    commit tree e6229b81005290a82604257924e780189915b7f7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.querySelector("#main").remove();
+// This selects a first main ID to remove 
+
+const newHeader = document.createElement("h1")
+// This creates a new header in the document in the DOM
+
+newHeader.id = "victory";
+// Giving the new header an ID
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+// 
